fa39a013e064a00e90f9429d71ddf016aa3c2b70
Alessandro Dal Bello <alessandro.dalbello@gmail.com>
2020-10-31 23:25:06 +0100

    Refactor tests of Haskell - Leap

--- haskell/leap/test/LeapYearSpec.hs
+++ haskell/leap/test/LeapYearSpec.hs
@@ -1,8 +1,5 @@
-{-# OPTIONS_GHC -fno-warn-type-defaults #-}
-{-# LANGUAGE RecordWildCards #-}
-
-import Data.Foldable (for_)
-import Test.Hspec    (Spec, describe, hspec, it, shouldBe)
+import Test.Hspec      (Spec, describe, hspec, it, shouldBe)
+import Test.QuickCheck (Gen, choose, forAll, property, suchThat)
 
 import LeapYear (isLeapYear)
 
@@ -10,53 +7,19 @@ main :: IO ()
 main = hspec specs
 
 specs :: Spec
-specs = describe "isLeapYear" $ for_ cases test
-  where
-    test Case{..} = it explanation assertion
-      where
-        explanation = unwords [show input, "-", description]
-        assertion   = isLeapYear (fromIntegral input) `shouldBe` expected
+specs = describe "isLeapYear" $ do
+          it "years not divisible by 4 should not be leap" $ property $
+            forAll (genYear (\year -> year `rem` 4 /= 0))
+              (\year -> isLeapYear year == False)
+          it "years divisible by 4 and by 100, but not by 400, should not be leap" $ property $
+            forAll (genYear (\year -> year `rem` 4 == 0 && year `rem` 100 == 0 && year `rem` 400 /= 0))
+              (\year -> isLeapYear year == False)
+          it "years divisible by 4 and not by 100 should be leap" $ property $
+            forAll (genYear (\year -> year `rem` 4 == 0 && year `rem` 100 /= 0))
+              (\year -> isLeapYear year == True)
+          it "years divisible by 4 and by 400 should be leap" $ property $
+            forAll (genYear (\year -> year `rem` 4 == 0 && year `rem` 400 == 0))
+              (\year -> isLeapYear year == True)
 
-data Case = Case { description :: String
-                 , input       :: Integer
-                 , expected    :: Bool
-                 }
-
-cases :: [Case]
-cases = [ Case { description = "year not divisible by 4 should be a common year"
-               , input       = 2015
-               , expected    = False
-               }
-        , Case { description = "year divisible by 2, not divisible by 4 should be a common year"
-               , input       = 1970
-               , expected    = False
-               }
-        , Case { description = "year divisible by 4, not divisible by 100 should be a leap year"
-               , input       = 1996
-               , expected    = True
-               }
-        , Case { description = "year divisible by 4 and 5 should be a leap year"
-               , input       = 1960
-               , expected    = True
-               }
-        , Case { description = "year divisible by 100, not divisible by 400 should be a common year"
-               , input       = 2100
-               , expected    = False
-               }
-        , Case { description = "year divisible by 100 but not by 3 should be a common year"
-               , input       = 1900
-               , expected    = False
-               }
-        , Case { description = "year divisible by 400 should be a leap year"
-               , input       = 2000
-               , expected    = True
-               }
-        , Case { description = "year divisible by 400 but not by 125 should be a leap year"
-               , input       = 2400
-               , expected    = True
-               }
-        , Case { description = "year divisible by 200, not divisible by 400 should be a common year"
-               , input       = 1800
-               , expected    = False
-               }
-        ]
+genYear :: (Integer -> Bool) -> Gen Integer
+genYear = suchThat $ choose (-5000, 10000)
