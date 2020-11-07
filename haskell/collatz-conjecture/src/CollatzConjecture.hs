008eda637c28ead62ac55dc03e1af7632c0197e5
Alessandro Dal Bello <alessandro.dalbello@gmail.com>
2020-11-07 19:14:04 +0100

    Refine Haskell - Collatz Conjecture

--- haskell/collatz-conjecture/src/CollatzConjecture.hs
+++ haskell/collatz-conjecture/src/CollatzConjecture.hs
@@ -1,10 +1,10 @@
 module CollatzConjecture (collatz) where
 
 collatz :: Integral a => a -> Maybe a
-collatz n = if n <= 0 then Nothing else collatz' n 0
+collatz n = if n <= 0 then Nothing else Just (collatz' n 0)
 
-collatz' :: Integral a => a -> a -> Maybe a
+collatz' :: Integral a => a -> a -> a
 collatz' n steps
-    | n == 1    = Just steps
+    | n == 1    = steps
     | even n    = collatz' (n `div` 2) (steps + 1)
     | otherwise = collatz' (n * 3 + 1) (steps + 1)
