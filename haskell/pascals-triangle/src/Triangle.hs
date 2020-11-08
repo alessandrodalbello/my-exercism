5e6ec3c8ba0587912d5fc99ff5c29d9a4e0f2b9c
Alessandro Dal Bello <alessandro.dalbello@gmail.com>
2020-11-08 17:17:23 +0100

    Refine Haskell - Pascal's Triangle

--- haskell/pascals-triangle/src/Triangle.hs
+++ haskell/pascals-triangle/src/Triangle.hs
@@ -5,5 +5,5 @@ rows n
     | n == 0    = []
     | n == 1    = [[1]]
     | otherwise = let prevTriangle = rows (n - 1)
-                      prevRow = (0 : last prevTriangle) ++ [0]
-                   in prevTriangle ++ [zipWith (+) prevRow (tail prevRow)]
+                      prevRow = last prevTriangle
+                   in prevTriangle ++ [zipWith (+) (0 : prevRow) (prevRow ++ [0])]
