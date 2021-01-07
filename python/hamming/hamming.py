ae5cd1640255aecc83230cef1da2ba8f44cf9e03
Alessandro Dal Bello <alessandro.dalbello@gmail.com>
2021-01-07 09:15:20 +0100

    Refine Python - Hamming

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,12 +1,5 @@
 def distance(strand_a, strand_b):
-    l = len(strand_a)
-    if (len(strand_b) != l):
+    if (len(strand_a) != len(strand_b)):
         raise ValueError("It is not possible to compare the strands. They must have the same length.")
 
-    distance = 0
-    i = 0
-    while i < l:
-        if strand_a[i] != strand_b[i]:
-            distance += 1
-        i += 1
-    return distance
+    return sum(a != b for a, b in zip(strand_a, strand_b))
