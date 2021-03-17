b1390481e13c0aef7c38efc9a6ba9752949d2c3c
Alessandro Dal Bello <alessandro.dalbello@gmail.com>
2021-03-17 21:09:57 +0100

    Refine Python - Twelve Days

--- python/twelve-days/twelve_days.py
+++ python/twelve-days/twelve_days.py
@@ -10,9 +10,12 @@ gifts = [
 ]
 
 def recite(start_verse, end_verse):
-    if start_verse not in range(1, 13) or end_verse not in range(1, 13) or start_verse > end_verse:
+    if not __is_verse_in_range(start_verse) or not __is_verse_in_range(end_verse) or start_verse > end_verse:
         raise ValueError("Invalid input.")
     return [ __recite(v) for v in range(start_verse - 1, end_verse) ]
+
+def __is_verse_in_range(verse):
+    return verse > 0 and verse <= len(ordinals)
 
 def __recite(verse):
     return f"On the {ordinals[verse]} day of Christmas my true love gave to me: {__recite_gifts(verse)}."
